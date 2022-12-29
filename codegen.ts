527da4944ccb72095b6317a2dbfca008f91f72ea
Edson Lucas <47072312+EdsonLucasbd@users.noreply.github.com>
2022-12-29 18:03:02 -0300

    change graphql codegen generated directory and file

--- codegen.ts
+++ codegen.ts
@@ -6,10 +6,10 @@ const config: CodegenConfig = {
     'https://api-sa-east-1.hygraph.com/v2/clc5nyfrg0rwp01t6e5zgdakw/master',
   documents: 'src/graphql/queries.ts',
   generates: {
-    'src/graphql/generated/': {
-      preset: 'client',
+    'src/graphql/generated/graphql.tsx': {
       plugins: [
         'typescript',
+        'typescript-operations',
         {
           add: {
             content: '/* eslint-disable */'
