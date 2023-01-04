ca43cf04e5994a644ba7c8e8ddc56ace21f1266f
Edson Lucas <47072312+EdsonLucasbd@users.noreply.github.com>
2023-01-04 08:05:52 -0300

    maxBounds & maxBoundsViscosity

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -42,6 +42,11 @@ const Map = ({ stadiums }: MapProps) => {
         center={[0, 0]}
         zoom={3}
         minZoom={3}
+        maxBounds={[
+          [-180, 180],
+          [180, -180]
+        ]}
+        maxBoundsViscosity={1.0}
         style={{ height: '100%', width: '100%' }}
       >
         <CustomTileLayer />
